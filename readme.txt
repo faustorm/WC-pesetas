0d1d9ec608b9eab2d179a3ed3ec09111ed0626f5
faustorm <faustoruizmadrid@gmail.com>
2020-05-22 04:21:08 +0200

    check if woocommerce is installed

---
+++ readme.txt
@@ -0,0 +1,34 @@
+=== Woo-pesetas ===
+Contributors: faustorm
+Tags: woocommerce,currency
+Requires at least: 4.0
+Tested up to: 5.4.1
+Requires PHP: 5.0
+Stable tag: trunk
+
+Wordpress plugin that adds the \"Peseta\" (the Spanish currency before the Euro) as a currency to Woocommerce.
+
+== Description ==
+Wordpress plugin that adds the \"Peseta\" (the Spanish currency before the Euro) as a currency to Woocommerce. Requires Woocommerce.
+
+If you have any reason to add the Peseta as a currency in your Woocommerce, with this plugin you can do it easily.
+
+##Why add the peseta as currency in your Woocommerce?
+
+You will not be able to charge in pesetas, since it is not a legal tender, but you can show how much the price of your products would have been in pesetas as a curiosity.
+
+== Frequently Asked Questions ==
+Will my clients be able to pay in Pesetas?
+No, the peseta is not a legal tender, you must collect in Euros, Dollars or any other currency.
+
+== Screenshots ==
+1. Now you can select the \"Peseta\" as your currency
+
+== Frequently Asked Questions ==
+Will my clients be able to pay in Pesetas?
+No, the peseta is not a legal tender, you must collect in Euros, Dollars or any other currency.
+
+== Changelog ==
+*0.1
+**Tested for WordPress 5.4.1
+**Tested for Woocommerce 4.1.1
--- woo-pesetas.php
+++ woo-pesetas.php
@@ -2,24 +2,51 @@
 
 /*
 Plugin Name: Woo-pesetas
-Plugin URI: https://faus.to
-Description: Añade pesetas como moneda a Woocommerce
+Plugin URI: https://faus.to/wordpress/plugins/woo-pesetas
+Description: Add "Peseta" as currency to Woocommerce
 Version: 0.1
-Author: Fausto Ruiz Madrid
+Author: Fausto Madrid
 Author URI: https://faus.to
 License: GPL
 */
-add_filter( 'woocommerce_currencies', 'add_cw_currency' );
-function add_cw_currency( $cw_currency ) {
-     $cw_currency['PESETA'] = __( 'Peseta', 'woocommerce' );
-     return $cw_currency;
-}
+//Check if woocommerce is active
+include_once(ABSPATH.'wp-admin/includes/plugin.php');
+if ( is_plugin_active( 'woocommerce/woocommerce.php' ) ){
 
-add_filter('woocommerce_currency_symbol', 'add_cw_currency_symbol', 10, 2);
-function add_cw_currency_symbol( $custom_currency_symbol, $custom_currency ) {
-     switch( $custom_currency ) {
-         case 'PESETA': $custom_currency_symbol = 'ptas.'; break;
-     }
-     return $custom_currency_symbol;
+	//Creates the currency
+	add_filter( 'woocommerce_currencies', 'woo_pesetas_add_currency_currency' );
+	function woo_pesetas_add_currency_currency( $woo_pesetas_currency ) {
+	     $woo_pesetas_currency['PESETA'] = __( 'Peseta', 'woocommerce' );
+	     return $woo_pesetas_currency;
+	}
+
+	//adds the currency and his symbol to the lists
+	add_filter('woocommerce_currency_symbol', 'woo_pesetas_add_currency_currency_symbol', 10, 2);
+	function woo_pesetas_add_currency_currency_symbol( $woo_pesetas_currency_symbol, $woo_pesetas_currency ) {
+	     switch( $woo_pesetas_currency ) {
+	         case 'PESETA': $woo_pesetas_currency_symbol = 'ptas.'; break;
+	     }
+	     return $woo_pesetas_currency_symbol;
+	}
 }
+//If Woocommerce is not active, show the error
+else {
+    function woo_pesetas_required_plugin() {
+        if ( is_admin() && current_user_can( 'activate_plugins' ) &&  !is_plugin_active( 'woocommerce/woocommerce.php' ) ) {
+            add_action( 'admin_notices', 'woo_pesetas_required_plugin_notice' );
+
+            deactivate_plugins( plugin_basename( __FILE__ ) ); 
+
+            if ( isset( $_GET['activate'] ) ) {
+                unset( $_GET['activate'] );
+            }
+        }
+
+    }
+    add_action( 'admin_init', 'woo_pesetas_required_plugin' );
+
+    function woo_pesetas_required_plugin_notice(){
+        ?><div class="error"><p>Error! You need to install or activate the <a target="_blank" href="https://es.wordpress.org/plugins/woocommerce/">Woocommerce</a> plugin before use "<span style="font-weight: bold;">Woo Pesetas</span>" plugin. Why are you trying to add a currency to Woocommerce without having Woocommerce?</p></div><?php
+    }
+}///
 ?>
